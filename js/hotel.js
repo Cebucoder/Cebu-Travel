de2f2c8ccdeb6362272c386092b17dbc6da4119f
CebuCoder <55612034+Cebucoder@users.noreply.github.com>
2023-01-23 05:25:52 -0800

    Add files via upload

---
+++ js/hotel.js
@@ -0,0 +1,36 @@
+function qesthotel() {
+  document.getElementById("qesthotel").src = "../images/qesthotel.webp";
+}
+function bayfront() {
+  document.getElementById("qesthotel").src = "../images/bayfront.webp";
+}
+function baihotel() {
+  document.getElementById("qesthotel").src = "../images/baihotel.webp";
+}
+function dusit() {
+  document.getElementById("qesthotel").src = "../images/dusit.webp";
+}
+function noble() {
+  document.getElementById("qesthotel").src = "../images/noble.webp";
+}
+function parklane() {
+  document.getElementById("qesthotel").src = "../images/parklane.webp";
+}
+function alicia() {
+  document.getElementById("qesthotel").src = "../images/alicia.webp";
+}
+function radison() {
+  document.getElementById("qesthotel").src = "../images/radison.jpg";
+}
+function soli() {
+  document.getElementById("qesthotel").src = "../images/soli.webp";
+}
+function sovoy() {
+  document.getElementById("qesthotel").src = "../images/sovoy.webp";
+}
+function waterfront() {
+  document.getElementById("qesthotel").src = "../images/waterfront.webp";
+}
+
+// menu bar Function
+
